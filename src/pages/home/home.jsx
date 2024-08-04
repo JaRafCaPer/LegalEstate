0077411c56ac2e194632a97205b5e60905096dec
Javier Caballero <caballeroperezjavier@gmail.com>
2024-08-04 13:03:55 -0500

    Se arregla error en la pagina home por componentes sin utilizar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,6 @@ import Header from '../../components/Header/Header.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
 import AboutUs from '../../components/AboutUs/AboutUs.jsx';
 import Services from '../../components/Services/Services.jsx';
-import Team from '../../components/Team/team.jsx';
-import Contact from '../../components/Contact/Contact.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 import imgLogo from '../../assets/images/12.png';
 import './home.css';
